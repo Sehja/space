77ecc8c64c5264c2c48a131480de356846c2d19d
Gaya <sehja@yandex.ru>
2021-11-02 22:21:10 +0300

    slider adaptive

--- js/main.js
+++ js/main.js
@@ -5,7 +5,18 @@ $(function () {
             slidesToShow: 1,
             slidesToScroll: 1,
             prevArrow: '<img src="images/arrow-left.png" alt="arrow-prev" class="slider__arrow-prev">',
-            nextArrow: '<img src="images/arrow-right.png" alt="arrow-next" class="slider__arrow-next">'
+            nextArrow: '<img src="images/arrow-right.png" alt="arrow-next" class="slider__arrow-next">',
+            responsive: [
+                {
+                    breakpoint: 1240,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '5px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
         });
     });
 });
